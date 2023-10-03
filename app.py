218b1d585ca48094ff3b45734cd95f2bd8a1f790
desertrambler <ccrenshaw2012@gmail.com>
2023-10-03 18:14:14 -0500

    made styling actually work

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+with app.test_request_context():
+    url_for('static', filename='styles.css')
+
+
