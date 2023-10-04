0c3de0d577ebcfa24988164749bfb8da67c628ad
desertrambler <ccrenshaw2012@gmail.com>
2023-10-04 07:20:04 -0500

    added images and corrected colors

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, abort, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def render_index():
     return render_template("index.html")
 
 with app.test_request_context():
     url_for('static', filename='styles.css')
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True) # make sure port is > 5000
 
